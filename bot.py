677ed8635ac6a0f6d2b86c453458511efcfc7907
Flamaster12 <flamaster12@inbox.ru>
2026-02-16 18:09:54 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -5,14 +5,20 @@ bot = telebot.TeleBot(TOKEN)
 
 # ---------- trimite plan ----------
 def send_plan(chat_id):
+    # Mesaj inițial
     bot.send_message(chat_id, "Iată un exemplu de plan 2D. Informațiile sunt în română și rusă.")
-    bot.send_document(chat_id, open("planexemplu.pdf", "rb"))
+    
+    # Trimite PDF
+    with open("planexemplu.pdf", "rb") as file:
+        bot.send_document(chat_id, file)
+    
+    # Mesaj final
     bot.send_message(
         chat_id,
-        "Mulțumim pentru interes!\n\n"
+        "Mulțumim pentru interesul tău!\n\n"
         "Prețul este 15 EUR pe metru pătrat.\n"
         "Avem și variante pentru orice buget, începând de la 3 EUR/m².\n\n"
-        "Scrie-ne aici pentru ofertă personalizată."
+        "Scrie-ne aici, sau la +37379044935, pentru ofertă personalizată."
     )
 
 # ---------- START (link de pe site) ----------
